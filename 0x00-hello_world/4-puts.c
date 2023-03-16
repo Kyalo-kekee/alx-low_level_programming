672258097acd96769e3258e212f8b5e3d53a3ede
Alex Kekee <kekekuza@gmail.com>
2023-03-16 06:47:20 -0700

    Print to stdout using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - print to stdout using puts
+ * Return: 0
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\"");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
